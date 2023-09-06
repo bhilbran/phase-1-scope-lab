d533519f2151b699ac5ae43c8958c9bc1d5dbcad
brooke <bhilbran@asu.edu>
2023-09-06 13:12:51 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName.toUpperCase();
+  }
+  
+  function setBestCustomer() {
+    return bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+  }
+ const leastFavoriteCustomer = 'bobby'
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'billy'
+}
